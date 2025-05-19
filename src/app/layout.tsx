5c012a7b9b2c36248db7e4d68ba96c1ca1ec2a67
o_khirano <hirano.keitaro.64x@gmail.com>
2025-05-18 19:57:00 -0700

    update/comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -113,7 +113,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </div>
           </aside>
 
-          {/* メインコンテンツ */}
+          {/* Main Content */}
           <main className="flex-1 p-6 overflow-y-auto">{children}</main>
         </div>
       </body>
